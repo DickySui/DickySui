f914aed91bc18b9b31d1ff419f4440949691e94a
DickySui <memedicky3@gmail.com>
2026-07-16 20:51:58 +0200

    Update page.js

--- app/page.js
+++ app/page.js
@@ -2,7 +2,14 @@ export default function Home() {
   return (
     <main>
       <h1>DickySui</h1>
-<p>Bienvenido a mi nueva web DickySui</p>
+      <p>Bienvenido a mi nueva web</p>
+
+      <section>
+        <h2>Próximamente</h2>
+        <p>
+          Estamos creando algo especial.
+        </p>
+      </section>
     </main>
   );
 }
